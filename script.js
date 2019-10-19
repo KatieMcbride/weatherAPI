5a09d6f3f7d0c120d7e2ea37a31141dd33d433ba
Katie McBride <mcbride.katieb@gmail.com>
2019-10-18 22:29:28 -0500

    HTML layout

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
     /* raw weather object from api */
     var cityWeather = "https://openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"
     /* variables */
+    // var queryURL = queryURL = "https://api.openweathermap.org/data/2.5/weather?q=&appid=" + city +"&appid=" + APIKey;
+    var city = '';
+
     /* parsed weather object */
 
     /* utility functions */
@@ -11,8 +14,10 @@ $(document).ready(function(){
     /* render parsed data */
 
     /* event functions */
+    
     /* search button click */
     /* have city name  */
+
     /* send city name to a openweather api */
     /* set the weather info to the object returned (see raw data) */
 
